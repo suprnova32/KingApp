bdb128448c669d986f66cdded234f2a84cac9903
Patricio Cano <suprnova32@gmail.com>
2013-05-06 23:43:41 +0200

    Global Constants

---
+++ src/com/insomniware/kingapp/extras/MyConstants.java
@@ -0,0 +1,13 @@
+package com.insomniware.kingapp.extras;
+
+public class MyConstants {
+	
+	public static final int FIVE_MINUTES = 1000 * 60 * 5;
+	public static final String PREFS_NAME = "com.insomniware.kingapp";
+	public static final String EXTRA_KEY_LOCATION = "EXTRA_LOCATION";
+	public static final String EXTRA_KEY_RADIUS = "RADIUS";
+	public static final int DEFAULT_RADIUS = 80;
+	public static int MAX_DISTANCE = DEFAULT_RADIUS/2;
+	public static long MAX_TIME = 60 * 1000;
+
+}
