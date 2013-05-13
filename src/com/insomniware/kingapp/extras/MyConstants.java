c20e9237bad89686445d7ea4f69bbfc258dd44d6
Patricio Cano <suprnova32@gmail.com>
2013-05-13 11:55:04 +0200

    Reduced Time and Distance

--- src/com/insomniware/kingapp/extras/MyConstants.java
+++ src/com/insomniware/kingapp/extras/MyConstants.java
@@ -7,7 +7,7 @@ public class MyConstants {
 	public static final String EXTRA_KEY_LOCATION = "EXTRA_LOCATION";
 	public static final String EXTRA_KEY_RADIUS = "RADIUS";
 	public static final int DEFAULT_RADIUS = 80;
-	public static int MAX_DISTANCE = DEFAULT_RADIUS/2;
-	public static long MAX_TIME = 60 * 1000;
+	public static int MAX_DISTANCE = DEFAULT_RADIUS/4;
+	public static long MAX_TIME = 30 * 1000;
 
 }
